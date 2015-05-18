994ee7f7872a18cf37b253247197be4464edd23a
tianc2014 <ct7zeither@gmail.com>
2015-05-18 00:25:33 -0700

    Same initializations for different epsilon's

--- LR.m
+++ LR.m
@@ -27,7 +27,7 @@ SigmaStar = eye(D);
 invSigmaStar = inv(SigmaStar);
 
 % vary values of epsilon
-nEps = 10;
+nEps = 20;
 logEps = linspace(-3,3,nEps);
 epsilon = exp(logEps);
 
@@ -37,6 +37,8 @@ loglikDPSGLD = zeros(nRepeat,nEps);
 
 accSGLD = zeros(nRepeat,1);
 accDPSGLD = zeros(nRepeat,nEps);
+
+tic;
 
 for l = 1:nRepeat
     %Simulate data
@@ -52,11 +54,8 @@ for l = 1:nRepeat
     SGLDsamples = LR_SGLD(X,y);
 
     % Run DP-SGLD for logistic regression
-    for k = 1:nEps
-        DPSGLDsamples{k} = LR_DP_SGLD(X,y,epsilon(k));
-    end
-    
-    
+    DPSGLDsamples = LR_DP_SGLD(X,y,epsilon);
+
     % Generate test data with the same data size N
     XTest = [ones(N,1),mvnrnd(muDesg,SigmaDesg,N)];
     probTrueTest = exp(XTest*betaTrue)./(1+exp(XTest*betaTrue));
@@ -76,12 +75,14 @@ for l = 1:nRepeat
     
      % Prediction results for DP-SGLD for different epsilon
     for k = 1:nEps
-        betaSamples = DPSGLDsamples{k};
+        betaSamples = DPSGLDsamples(:,:,k);
         probPred = mean(exp(XTest*betaSamples')./(1+exp(XTest*betaSamples')),2);
         loglikDPSGLD(l,k) = sum(yTest.*log(probPred)+(1-yTest).*log(1-probPred));
         accDPSGLD(l,k) = mean((probPred > 0.5) == yTest);
     end
 end
+
+time = toc/nRepeat
 
 currenttime= datestr(now,'dd-mm-yy_HH_MM_SS');
 
--- LR_DP_SGLD.m
+++ LR_DP_SGLD.m
@@ -8,33 +8,35 @@ D = size(X,2); % parameter size
 
 tau = floor(sqrt(N)); % size of minibatch
 % epsilon = 0.5;
+nEps = size(epsilon,2);
 delta = 0.1;
 L = 0.8; % Lipschitz constant
 %T = epsilon^2*N/(32*tau*log(2/delta))+1;
 T = 2*N;
 alpha = 0.1;
-maxIter = floor(N*T/tau);
+%maxIter = floor(N*T/tau);
+maxIter = 10000;
 
 % Initialization with OPS
 B = 0.8;
-betaInitOPS = LR_OPS(X,y,B,epsilon);
+epsOPS = 0.1;
+betaInitOPS = LR_OPS(X,y,B,epsOPS);
+%betaInitOPS = LR_OPS(X,y,B,epsilon);
 
 % prior
 muStar = zeros(D,1);
 SigmaStar = eye(D);
 invSigmaStar = inv(SigmaStar);
 
-
 %% Initialize
 beta0 = betaInitOPS(end,:);
 %beta0 = rand(1,D);
 %beta0 = zeros(1,D);
-betaVec = [];
-betaVec(1,:) = beta0;
+betaArray = [];
+betaArray(1,:,:) = permute(repmat(beta0,nEps,1),[3 2 1]);
 
 eta = zeros(maxIter,1);
 z = zeros(maxIter,D);
-
 
 % simple random walk Metropolis
 for t = 1:maxIter
@@ -44,29 +46,37 @@ for t = 1:maxIter
     
     % sample coordinates of z
     % eta(t) = a*(b+t)^(-gamma);
-    eta(t) = alpha*epsilon^2/(128*L^2*log(2.5*N*T/tau/delta)*log(2/delta)*t);
-    etaBound = alpha*eta(t)/t*N*T/tau;
-    zVar = max(eta(t),etaBound);
-    
-    for i = 1:D
-        z(t,i) = normrnd(0,sqrt(zVar));
+    for i = 1:nEps
+        
+        eta(t) = alpha*epsilon(i)^2/(128*L^2*log(2.5*N*T/tau/delta)*log(2/delta)*t);
+        etaBound = alpha*eta(t)/t*N*T/tau;
+        zVar = max(eta(t),etaBound);
+        
+        for j = 1:D
+            z(t,j) = normrnd(0,sqrt(zVar));
+        end
+        gradR = invSigmaStar*(betaArray(t,:,i)'-muStar);
+        gradL = -X(S,:)'*(y(S)-exp(X(S,:)*betaArray(t,:,i)')./(1+exp(X(S,:)*betaArray(t,:,i)')));
+        
+        
+        betaArray(t+1,:,i) = betaArray(t,:,i)-eta(t)*(gradR+N/tau*gradL)'+z(t,:);
     end
-    gradR = invSigmaStar*(betaVec(t,:)'-muStar);
-    gradL = -X(S,:)'*(y(S)-exp(X(S,:)*betaVec(t,:)')./(1+exp(X(S,:)*betaVec(t,:)')));
     
-    betaVec(t+1,:) = betaVec(t,:)-eta(t)*(gradR+N/tau*gradL)'+z(t,:);
-    
+    a = 1;
 end
 
 if plotInd == 1
-    figure;
-    plotDim = ceil(sqrt(D));
-    for k = 1:D
-        subplot(plotDim,plotDim,k);
-        plot(betaVec(:,k));
+    for i = 1:nEps
+        figure;
+        plotDim = ceil(sqrt(D));
+        for j = 1:D
+            subplot(plotDim,plotDim,j);
+            plot(betaArray(:,j,i));
+            title(['$\beta_' num2str(j) '$ ($\epsilon = $' num2str(epsilon(i)) ')']);
+        end
     end
 end
 
 burnIn = maxIter - (1-alpha)*N*T/tau;
-samples = betaVec(burnIn+1:end,:);
+samples = betaArray(burnIn+1:end,:,:);
 end
